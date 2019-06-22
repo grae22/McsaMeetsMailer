fbb3bdd44f2b05ca906c0cbdcdb68eb712bc3095
GraemeB <grae22@gmail.com>
2019-06-22 09:37:31 +0200

    ApiExceptionFilter and related classes added.

---
+++ McsaMeetsMailer/ErrorHandling/ApiError.cs
@@ -0,0 +1,35 @@
+﻿// Adapted from https://weblog.west-wind.com/posts/2016/oct/16/error-handling-and-exceptionfilter-dependency-injection-for-aspnet-core-apis
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace McsaMeetsMailer.ErrorHandling
+{
+  public class ApiError
+  {
+    public string Message { get; set; }
+    public bool IsError { get; set; }
+    public string Detail { get; set; }
+    //public ValidationErrorCollection Errors { get; set; }
+
+    public ApiError(string message)
+    {
+      Message = message;
+      IsError = true;
+    }
+
+    public ApiError(ModelStateDictionary modelState)
+    {
+      IsError = true;
+
+      if (modelState != null &&
+          modelState.Any(m => m.Value.Errors.Count > 0))
+      {
+        Message = "Please correct the specified errors and try again.";
+        //errors = modelState.SelectMany(m => m.Value.Errors).ToDictionary(m => m.Key, m=> m.ErrorMessage);
+        //errors = modelState.SelectMany(m => m.Value.Errors.Select( me => new KeyValuePair<string,string>( m.Key,me.ErrorMessage) ));
+        //errors = modelState.SelectMany(m => m.Value.Errors.Select(me => new ModelError { FieldName = m.Key, ErrorMessage = me.ErrorMessage }));
+      }
+    }
+  }
+}
