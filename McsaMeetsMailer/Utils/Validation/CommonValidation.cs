ea2c957e04ec265f20b2c98afedcc9884e076c6f
GraemeB <grae22@gmail.com>
2019-06-22 18:55:25 +0200

    Adding common validation for null args.

---
+++ McsaMeetsMailer/Utils/Validation/CommonValidation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace McsaMeetsMailer.Utils.Validation
+{
+  public static class CommonValidation
+  {
+    public static void RaiseExceptionIfArgumentNull(in object argument, in string argumentName)
+    {
+      if (argument != null)
+      {
+        return;
+      }
+
+      throw new ArgumentNullException(argumentName);
+    }
+  }
+}
---
+++ McsaMeetsMailerTests/Utils/Validation/CommonValidationTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using McsaMeetsMailer.Utils.Validation;
+using NSubstitute.ExceptionExtensions;
+using NUnit.Framework;
+
+namespace McsaMeetsMailerTests.Utils.Validation
+{
+  [TestFixture]
+  public class CommonValidationTests
+  {
+    [Test]
+    public void RaiseExceptionIfArgumentNull_GivenNullArgument_ShouldRaiseArgumentNullException()
+    {
+      // Arrange.
+      // Act.
+      // Assert.
+      Assert.Throws<ArgumentNullException>(
+        () => CommonValidation.RaiseExceptionIfArgumentNull(null, "SomeName"));
+    }
+
+    [Test]
+    public void RaiseExceptionIfArgumentNull_GivenNotNullArgument_ShouldNotRaiseException()
+    {
+      // Arrange.
+      // Act.
+      CommonValidation.RaiseExceptionIfArgumentNull("SomeValue", "SomeName");
+
+      // Assert.
+      Assert.Pass();
+    }
+  }
+}
