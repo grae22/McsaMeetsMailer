d34561a17da929bae63a7f124967ea28ac01085c
melzatkinson <melzatkinson@gmail.com>
2019-06-24 22:11:44 +0200

    Fix

---
+++ McsaMeetsMailer/Models/MeetDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace McsaMeetsMailer.Models
+{
+  public class MeetDetailsModel
+  {
+    public string Leader { get; set; }
+    public string LeaderEmail { get; set; }
+    public Dictionary<string, string> AdditionalFields { get; set; }
+  }
+}
