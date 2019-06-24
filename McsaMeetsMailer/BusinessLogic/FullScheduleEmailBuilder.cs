faaafc762503b5120c67a9af7fbd65bbe1f47fdc
melzatkinson <melzatkinson@gmail.com>
2019-06-24 22:41:07 +0200

    HtmlBuilder tests added.

--- McsaMeetsMailer/BusinessLogic/FullScheduleEmailBuilder.cs
+++ McsaMeetsMailer/BusinessLogic/FullScheduleEmailBuilder.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using McsaMeetsMailer.Models;
-using McsaMeetsMailer.Utils.HtmlBuilder;
+using McsaMeetsMailer.Utils.Html;
 
-namespace McsaMeetsMailer.Services
+namespace McsaMeetsMailer.BusinessLogic
 {
   public class FullScheduleEmailBuilder
   {
--- McsaMeetsMailer/Utils/Html/IHtmlBuilder.cs
+++ McsaMeetsMailer/Utils/Html/IHtmlBuilder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace McsaMeetsMailer.Utils.HtmlBuilder
+namespace McsaMeetsMailer.Utils.Html
 {
   public interface IHtmlBuilder
   {
