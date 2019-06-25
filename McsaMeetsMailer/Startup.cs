b23db59377c00c03094fa74b2be00afdab0c4b06
GraemeB <grae22@gmail.com>
2019-06-25 14:30:06 +0200

    DI setup for meets service.

--- McsaMeetsMailer/Startup.cs
+++ McsaMeetsMailer/Startup.cs
@@ -1,11 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using McsaMeetsMailer.BusinessLogic;
+using McsaMeetsMailer.Services;
+using McsaMeetsMailer.Utils.Logging;
+using McsaMeetsMailer.Utils.RestRequest;
+using McsaMeetsMailer.Utils.Settings;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,13 +27,27 @@ namespace McsaMeetsMailer
     {
       services.Configure<CookiePolicyOptions>(options =>
       {
-              // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-              options.CheckConsentNeeded = context => true;
+        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+        options.CheckConsentNeeded = context => true;
         options.MinimumSameSitePolicy = SameSiteMode.None;
       });
 
-
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+      var logger = new DummyLogger();
+      var requestMaker = new WebRestRequestMaker();
+      var settings = new EnvironmentVariableSettings();
+      var meetSheetFactory = new MeetsGoogleSheetFactory();
+
+      var meetsService = new MeetsService(
+        settings,
+        requestMaker,
+        meetSheetFactory,
+        logger);
+
+      services.AddSingleton<ILogger>(logger);
+      services.AddSingleton<ISettings>(settings);
+      services.AddSingleton<IMeetsService>(meetsService);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
