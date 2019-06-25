3a6f1669204ca310b1e5ee10814c1a2ad23ad1aa
GraemeB <grae22@gmail.com>
2019-06-25 13:24:08 +0200

    WIP MeetsService.

---
+++ McsaMeetsMailer/Services/MeetsService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using McsaMeetsMailer.BusinessLogic;
+using McsaMeetsMailer.Models;
+using McsaMeetsMailer.Utils.Logging;
+using McsaMeetsMailer.Utils.RestRequest;
+using McsaMeetsMailer.Utils.Settings;
+
+namespace McsaMeetsMailer.Services
+{
+  public class MeetsService : IMeetsService
+  {
+    private const string LoggingClassName = "[MeetsService]";
+    private const string SettingName_MeetsGoogleSheetId = "MCSA-KZN_Meets_MeetsGoogleSheetId";
+    private const string SettingName_GoogleAppKey = "MCSA-KZN_Meets_GoogleAppKey";
+    private const string GoogleSheetsBaseUrl = "https://sheets.googleapis.com/v4/spreadsheets/";
+
+    private readonly string _meetsGoogleSheetId;
+    private readonly string _googleAppKey;
+    private readonly IRestRequestMaker _requestMaker;
+    private readonly ILogger _logger;
+
+    public MeetsService(
+      in ISettings settings,
+      in IRestRequestMaker requestMaker,
+      in ILogger logger)
+    {
+      if (settings == null)
+      {
+        throw new ArgumentNullException(nameof(settings));
+      }
+
+      _requestMaker = requestMaker ?? throw new ArgumentNullException(nameof(requestMaker));
+      _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+      _meetsGoogleSheetId = settings.GetValidValue(SettingName_MeetsGoogleSheetId);
+      _googleAppKey = settings.GetValidValue(SettingName_GoogleAppKey);
+    }
+
+    public async Task<IEnumerable<MeetDetailsModel>> RetrieveMeets()
+    {
+      var url = $"{GoogleSheetsBaseUrl}{_meetsGoogleSheetId}/values/Sheet1!A1:Z500?key={_googleAppKey}";
+
+      Uri uri;
+
+      try
+      {
+        uri = new Uri(url);
+      }
+      catch (UriFormatException ex)
+      {
+        _logger.LogError($"{LoggingClassName} Failed to build URI from URL \"{url}\".", ex);
+        return null;
+      }
+
+      _logger.LogDebug($"{LoggingClassName} Retrieving meets from \"{url}\"...");
+
+      MeetsGoogleSheet sheet = await MeetsGoogleSheet.Retrieve(
+        uri,
+        _requestMaker,
+        _logger);
+
+      if (sheet == null)
+      {
+        _logger.LogError($"{LoggingClassName} Failed to retrieve meets.");
+        return null;
+      }
+
+      _logger.LogDebug($"{LoggingClassName} Retrieved meets, transforming into models...");
+
+      GoogleSheetToMeetDetailsTransformer.Process(
+        sheet,
+        out IEnumerable<MeetDetailsModel> meetDetailsModels);
+
+      return meetDetailsModels;
+    }
+  }
+}
---
+++ McsaMeetsMailerTests/Services/MeetsServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using McsaMeetsMailer.Services;
+using McsaMeetsMailer.Utils.Logging;
+using McsaMeetsMailer.Utils.RestRequest;
+using McsaMeetsMailer.Utils.Settings;
+
+using NSubstitute;
+
+using NUnit.Framework;
+
+namespace McsaMeetsMailerTests.Services
+{
+  [TestFixture]
+  public class MeetsServiceTests
+  {
+    private const string SettingName_MeetsGoogleSheetId = "MCSA-KZN_Meets_MeetsGoogleSheetId";
+    private const string SettingName_GoogleAppKey = "MCSA-KZN_Meets_GoogleAppKey";
+
+    [Test]
+    [Ignore("WIP")]
+    public async Task RetrieveMeets_GivenHappyPath_ShouldReturnNotNullMeetsCollection()
+    {
+      // Arrange.
+      var settings = Substitute.For<ISettings>();
+      var requestMaker = Substitute.For<IRestRequestMaker>();
+      var logger = Substitute.For<ILogger>();
+
+      settings
+        .GetValidValue(SettingName_MeetsGoogleSheetId)
+        .Returns("SomeSheetId");
+
+      settings
+        .GetValidValue(SettingName_GoogleAppKey)
+        .Returns("SomeAppKey");
+
+
+
+      var testObject = new MeetsService(
+        settings,
+        requestMaker,
+        logger);
+
+      // Act.
+      var result = await testObject.RetrieveMeets();
+
+      // Assert.
+      Assert.IsNotNull(result);
+    }
+  }
+}
