fa0bd2ad08701792f671590fc097b4d1e751d933
GraemeB <grae22@gmail.com>
2019-06-25 14:30:21 +0200

    MeetsService integrated into full preview page.

--- McsaMeetsMailer/Pages/FullSchedulePreview.cshtml.cs
+++ McsaMeetsMailer/Pages/FullSchedulePreview.cshtml.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using McsaMeetsMailer.Models;
 using McsaMeetsMailer.BusinessLogic;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using McsaMeetsMailer.Services;
 using McsaMeetsMailer.Utils.Html;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace McsaMeetsMailer.Pages
 {
@@ -14,23 +13,41 @@ namespace McsaMeetsMailer.Pages
   {
     public string html;
 
-    public void OnGet()
-    {
-      var collection = new List<MeetDetailsModel>
-          {
-            new MeetDetailsModel()
-            {
-              Leader = "Graeme",
-              LeaderEmail = "grae22@gmail.com",
-              AdditionalFields = new Dictionary<string, string>()
-              {
-                {"StartDate", new DateTime( 2019, 8, 5, 8, 0, 0 ).Date.ToString()},
-                {"EndDate", new DateTime( 2019, 8, 5, 5, 0, 0 ).Date.ToString()}
-              }
-            },
-          };
+    private readonly IMeetsService _meetsService;
 
-      html = FullScheduleEmailBuilder.Build( collection, new HtmlBuilder() );
+    public FullSchedulePreviewModel(IMeetsService meetsService)
+    {
+      _meetsService = meetsService ?? throw new ArgumentNullException(nameof(meetsService));
+    }
+
+    public async Task OnGet()
+    {
+#if false
+      var collection = new List<MeetDetailsModel>
+      {
+        new MeetDetailsModel()
+        {
+          Leader = "Graeme",
+          LeaderEmail = "grae22@gmail.com",
+          AdditionalFields = new Dictionary<string, string>()
+          {
+            {"StartDate", new DateTime( 2019, 8, 5, 8, 0, 0 ).Date.ToString()},
+            {"EndDate", new DateTime( 2019, 8, 5, 5, 0, 0 ).Date.ToString()}
+          }
+        },
+      };
+
+      html = FullScheduleEmailBuilder.Build(collection, new HtmlBuilder());
+#else
+      IEnumerable<MeetDetailsModel> meets = await _meetsService.RetrieveMeets();
+
+      if (meets == null)
+      {
+        return;
+      }
+
+      html = FullScheduleEmailBuilder.Build(meets, new HtmlBuilder());
+#endif
     }
   }
 }
