a89b4239a2a7961f04523dc419cd5ee9e37da706
GraemeB <grae22@gmail.com>
2019-07-03 10:06:08 +0200

    Test controller added.

---
+++ McsaMeetsMailer/Controllers/TestController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using McsaMeetsMailer.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace McsaMeetsMailer.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TestController : ControllerBase
+  {
+    private readonly IEmailAddressService _emailAddressService;
+
+    public TestController(IEmailAddressService emailAddressService)
+    {
+      _emailAddressService = emailAddressService ?? throw new ArgumentNullException(nameof(emailAddressService));
+    }
+
+    [Route("retrieveEmailAddresses")]
+    public async Task<ActionResult> RetrieveEmailAddresses()
+    {
+      try
+      {
+        await _emailAddressService.RetrieveEmailAddresses();
+      }
+      catch (Exception)
+      {
+        return StatusCode(500);
+      }
+
+      return Ok();
+    }
+  }
+}
