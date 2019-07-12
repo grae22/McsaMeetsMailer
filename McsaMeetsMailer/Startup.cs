2bb23183c2a96b0974f339122c18fd25d77cef69
GraemeB <grae22@gmail.com>
2019-07-12 14:53:11 +0200

    Removing unneeded stuff.

--- McsaMeetsMailer/Startup.cs
+++ McsaMeetsMailer/Startup.cs
@@ -7,7 +7,6 @@ using McsaMeetsMailer.Utils.Settings;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,13 +25,6 @@ namespace McsaMeetsMailer
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.Configure<CookiePolicyOptions>(options =>
-      {
-        // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-        options.CheckConsentNeeded = context => true;
-        options.MinimumSameSitePolicy = SameSiteMode.None;
-      });
-
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
       var logger = new ConsoleLogger();
@@ -77,7 +69,6 @@ namespace McsaMeetsMailer
 
       app.UseHttpsRedirection();
       app.UseStaticFiles();
-      app.UseCookiePolicy();
 
       app.UseMvc();
     }
