b82c51dfd80b8b22036fda6bf9eb6d3347878fc5
GraemeB <grae22@gmail.com>
2019-07-20 09:53:16 +0200

    Meets page added - will be used by public to view schedule online.

---
+++ McsaMeetsMailer/Pages/Meets.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using McsaMeetsMailer.BusinessLogic;
+using McsaMeetsMailer.Models;
+using McsaMeetsMailer.Services;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace McsaMeetsMailer.Pages
+{
+  public class MeetsModel : PageModel
+  {
+    public string Html { get; private set; }
+
+    private readonly IMeetsService _meetsService;
+
+    public MeetsModel(IMeetsService meetsService)
+    {
+      _meetsService = meetsService ?? throw new ArgumentNullException(nameof(meetsService));
+    }
+
+    public async Task OnGet()
+    {
+      IEnumerable<MeetDetailsModel> meets = await _meetsService.RetrieveMeets(DateTime.Now);
+
+      if (meets == null)
+      {
+        return;
+      }
+
+      Html = FullScheduleEmailBuilder.Build(
+        meets,
+        string.Empty,
+        false);
+    }
+  }
+}
