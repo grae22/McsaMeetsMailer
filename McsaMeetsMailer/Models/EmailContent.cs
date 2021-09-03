8c2f0729c7a59eee16071e929effe7f6c464630b
Graeme Bruschi <grae22@gmail.com>
2021-09-03 13:48:56 +0200

    Add page for sending abridged meet sheets.

--- McsaMeetsMailer/Models/EmailContent.cs
+++ McsaMeetsMailer/Models/EmailContent.cs
@@ -5,5 +5,6 @@
     public string Address { get; set; }
     public string Subject { get; set; }
     public string Body { get; set; }
+    public string SendMeetsUpUntilThisDate { get; set; }
   }
 }
