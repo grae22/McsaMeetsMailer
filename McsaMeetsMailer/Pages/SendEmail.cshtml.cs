fd3cc4a87828b1badd61eb3ecf3286df1dae55f0
Graeme Bruschi <grae22@gmail.com>
2024-01-04 14:39:49 +0200

    Disable the 'send email' page with override.

--- McsaMeetsMailer/Pages/SendEmail.cshtml.cs
+++ McsaMeetsMailer/Pages/SendEmail.cshtml.cs
@@ -4,10 +4,12 @@ namespace McsaMeetsMailer.Pages
 {
   public class SendEmailModel : PageModel
   {
+    public bool OptionsEnabled { get; private set; }
     public string DebugOptionsVisibility { get; private set; }
 
     public void OnGet()
     {
+      OptionsEnabled = Request.Query.ContainsKey("enabled");
       DebugOptionsVisibility = Request.Query.ContainsKey("debug") ? "visible" : "hidden";
     }
   }
